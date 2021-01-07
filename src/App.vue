08cfd1b3be8d0bc325f5833445fa5a29c61f2d1b
cesargodoi <cesar.godoi@gmail.com>
2021-01-07 07:33:42 -0300

    get players

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,30 @@
 <template>
-  <div></div>
+  <!-- header -->
+  <h1>Tic-Tac-Toe</h1>
+  <!-- get players -->
+  <get-players v-if="step === 1" @setPlayers="setPlayers" />
 </template>
 
-<script></script>
+<script>
+import "./css/global.css";
+import GetPlayers from "./components/GetPlayers.vue";
+export default {
+  components: { GetPlayers },
+  data() {
+    return {
+      step: 1,
+      playerX: "",
+      playerO: "",
+    };
+  },
+  methods: {
+    setPlayers(playerX, playerO) {
+      this.playerX = playerX ? playerX : "Player X";
+      this.playerO = playerO ? playerO : "Player O";
+      this.step = 2;
+    },
+  },
+};
+</script>
 
 <style></style>
---
+++ src/components/GetPlayers.vue
@@ -0,0 +1,39 @@
+<template>
+  <input type="text" placeholder="player X" v-model="playerX" />
+  <input type="text" placeholder="player Y" v-model="playerO" />
+  <button @click="getPlayers">Start Game</button>
+</template>
+
+<script>
+export default {
+  name: "get-player",
+  emits: ["setPlayers"],
+  data() {
+    return {
+      playerX: "",
+      playerO: "",
+    };
+  },
+  methods: {
+    getPlayers() {
+      this.$emit("setPlayers", this.playerX, this.playerO);
+      this.playerX = "";
+      this.playerO = "";
+    },
+  },
+};
+</script>
+
+<style scoped>
+input {
+  border: 0;
+  border-radius: 9px;
+  width: 160px;
+  margin: 5px;
+  padding: 10px;
+  font-size: 18px;
+  font-weight: bold;
+  text-align: center;
+  color: var(--text-color);
+}
+</style>
